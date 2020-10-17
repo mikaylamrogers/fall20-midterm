8008d06d639cd7cfa5390c78632c31a5066d9c38
Mikayla Rogers <m.ikaylarogers@icloud.com>
2020-10-17 15:16:47 -0400

    successfully displayed artist names

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,24 +4,24 @@ import axios from 'axios';
 // import {  useHistory  } from 'react-router-dom';
 
 function Home() {
-    const [wikiArtData, setWikiArtData] = useState(null);
+    const [wikiArtData, setWikiArtData] = useState([]);
     // const [anotherDataSource, setAnotherDataSource] = useState([]);
 
     useEffect(() => {
         axios
-        .get("https://www.wikiart.org/en/app/api/popularartists?&authSessionKey=dc991ee37f18")
+        .get("https://cors-anywhere.herokuapp.com/https://www.wikiart.org/en/app/api/popularartists?&authSessionKey=dc991ee37f18")
             
         .then(function (response) {
             console.log('response', response)
-                // const artdataresponse = 
-                // setWikiArtData(artdataresponse);
+                const popularartistsresponse = response.data;
+                setWikiArtData(popularartistsresponse);
         })
         .catch(function (error) {
             console.warn(error);
         })
     }, []);
 
-
+    console.log("wikiArtData", wikiArtData);
 
     // useEffect(() => {   
     //     const searchParams = history.location.search;
@@ -37,12 +37,12 @@ function Home() {
     return (
         <div>
             <h1>Home: Midterm</h1>
-            {/* {wikiArtData.map((artistName, i) => (
+            {wikiArtData.map((artistName, i) => (
                 <div key={i}>
-                    <h2>{artistName.name}</h2>
-                    <p>Artist Name: {artistName.count}</p>
+                    <h2>{artistName.artistName}</h2>
+                    {/* <p>Artist Name: {artistName.url}</p> */}
                 </div>
-            ))} */}
+            ))}
         </div>
     );
 }
